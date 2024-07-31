352f55bc11bf7e82f1215621fa9018879f9bbd5b
Xursanjonov <nxursanjonov888@gmail.com>
2024-07-31 21:28:51 +0500

    add to Users CRUD and Backend

--- routes/users.js
+++ routes/users.js
@@ -9,13 +9,13 @@ router.get('/', async (req, res) => {
         const users = await User.find()
         if (!users.length) {
             return res.status(400).json({
-                message: 'Users is not defined',
+                message: 'Malumot topilmadi',
                 variant: 'Warning',
                 payload: null,
             })
         }
         res.status(200).json({
-            message: 'All Users',
+            message: 'Barcha Foydalanuvchilar',
             variant: 'Success',
             usersLength: users.length,
             totalBudget: users.reduce((total, user) => total + user.budget, 0),
@@ -35,13 +35,13 @@ router.get('/:id', async (req, res) => {
         const user = await User.findById(req.params.id)
         if (!user) {
             return res.status(400).json({
-                message: 'User is not defined',
+                message: 'Foydalanuvchi topilmadi',
                 variant: 'Warning',
                 payload: null,
             })
         }
         res.status(200).json({
-            message: 'User',
+            message: 'Foydalanuvchi',
             variant: 'Success',
             payload: user,
         })
@@ -60,7 +60,7 @@ router.post('/', async (req, res) => {
         const confligUser = allUsers.find((user) => user.username === req.body.username)
         if (confligUser) {
             return res.status(400).json({
-                message: 'User already exists',
+                message: 'Foydalanuvchi mavjud',
                 variant: 'Warning',
                 payload: null,
             })
@@ -69,7 +69,7 @@ router.post('/', async (req, res) => {
         allUsers = await User.find()
 
         res.status(201).json({
-            message: 'User created',
+            message: "Yangi foydalanuvchi qo'shildi",
             variant: 'Success',
             payload: newUser,
         })
@@ -87,14 +87,14 @@ router.delete('/:id', async (req, res) => {
         const user = await User.findById(req.params.id)
         if (!user) {
             return res.status(400).json({
-                message: 'User is not defined',
+                message: 'Foydalanuvchi topilmadi',
                 variant: 'Warning',
                 payload: null,
             })
         }
         await User.findByIdAndDelete(req.params.id)
         res.status(200).json({
-            message: 'User deleted',
+            message: 'Foydalanuvchi o`chirildi',
             variant: 'Success',
             payload: null,
         })
@@ -112,14 +112,14 @@ router.put('/:id', async (req, res) => {
         const user = await User.findById(req.params.id)
         if (!user) {
             return res.status(400).json({
-                message: 'User is not defined',
+                message: 'Foydalanuvchi topilmadi',
                 variant: 'Warning',
                 payload: null,
             })
         }
         const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body)
         res.status(200).json({
-            message: 'User updated',
+            message: 'Foydalanuvchi o`zgartirildi',
             variant: 'Success',
             payload: updatedUser,
         })
